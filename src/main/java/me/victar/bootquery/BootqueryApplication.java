28f95ce57fcb099078ae5d6a8601917ac75b8585
Victar Kadol <viktor.kadol@axamit.com>
2016-01-26 20:56:50 +0300

    Initial commit

---
+++ src/main/java/me/victar/bootquery/BootqueryApplication.java
@@ -0,0 +1,12 @@
+package me.victar.bootquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootqueryApplication.class, args);
+	}
+}
